292809340c099a4e2e27595b63d9a98947dab602
elined <167140433+miss-elined@users.noreply.github.com>
2025-04-14 01:27:48 +0300

    Create human choice

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ function getComputerChoice() {
   let random = Math.random();
   return random < 0.3 ? "Rock" : random < 0.6 ? "Paper" : "Scissors";
 }
+
+function getHumanChoice() {
+  let userChoice = prompt();
+  return userChoice;
+}
+
+// console.log(getHumanChoice());
