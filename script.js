2e4a0e0c068d605c87160fcc9ddd6a7bc8dad11b
elined <167140433+miss-elined@users.noreply.github.com>
2025-04-14 01:38:03 +0300

    Create function for one round

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getComputerChoice() {
   let random = Math.random();
-  return random < 0.3 ? "Rock" : random < 0.6 ? "Paper" : "Scissors";
+  return random < 0.3 ? "rock" : random < 0.6 ? "paper" : "scissors";
 }
 
 function getHumanChoice() {
@@ -9,3 +9,23 @@ function getHumanChoice() {
 }
 
 // console.log(getHumanChoice());
+
+let computerScore = 0;
+let humanScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice =
+    humanChoice[0].toUpperCase() + humanChoice.slice(1).toLowerCase();
+
+  if (
+    (computerChoice === "Rock" && humanChoice === "Scissors") ||
+    (computerChoice === "Paper" && humanChoice === "Rock") ||
+    (computerChoice === "Sciccors" && humanChoice === "Paper")
+  ) {
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    computerScore++;
+  } else {
+    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    humanScore++;
+  }
+}
