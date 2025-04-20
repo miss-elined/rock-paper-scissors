4dc5abb74c734de170b455274e7fff6dd1829a7d
elined <167140433+miss-elined@users.noreply.github.com>
2025-04-20 17:19:40 +0300

    Add UI

--- script.js
+++ script.js
@@ -1,55 +1,87 @@
+const buttons = document.querySelectorAll("button");
+let resultDiv = document.querySelector("#results");
+let end = document.querySelector("#end");
+
+function makeButtonsClickable() {
+  buttons.forEach((button) => {
+    button.addEventListener("click", playRound);
+  });
+}
+
+makeButtonsClickable();
+
+let computerScore = 0;
+let humanScore = 0;
+let ties = 0;
+
 function getComputerChoice() {
-  let random = Math.random();
-  return random < 0.3 ? "Rock" : random < 0.6 ? "Paper" : "Scissors";
+  let random = Math.round(Math.random() * 2);
+  return random === 0 ? "Rock" : random === 1 ? "Paper" : "Scissors";
 }
 
-// console.log(getComputerChoice());
+function resetGame() {
+  const body = document.querySelector("body");
+  const resetButton = document.createElement("button");
+  const buttonText = "Reset";
+  resetButton.innerText = buttonText;
+  resetButton.setAttribute("id", "reset");
+  body.appendChild(resetButton);
+  resetButton.addEventListener("click", zeroScore);
+  resetButton.addEventListener("click", makeButtonsClickable);
+  resetButton.addEventListener("click", (e) => e.target.remove());
 
-function getHumanChoice() {
-  let userChoice = prompt();
-  return userChoice;
+  function zeroScore() {
+    computerScore = 0;
+    humanScore = 0;
+    ties = 0;
+    resultDiv.innerText = "";
+    end.innerText = "";
+  }
 }
 
-// console.log(getHumanChoice());
+function playRound(e) {
+  let humanChoice = e.target.innerText;
+  let computerChoice = getComputerChoice();
+  let result = "";
+  makeButtonsClickable();
 
-// playRound(human, computer);
-
-function playGame() {
-  let computerScore = 0;
-  let humanScore = 0;
-  let ties = 0;
-
-  function playRound(humanChoice, computerChoice) {
-    humanChoice =
-      humanChoice[0].toUpperCase() + humanChoice.slice(1).toLowerCase();
-    // console.log(computerChoice);
-    // console.log(humanChoice);
-    if (
+  if (
       (computerChoice === "Rock" && humanChoice === "Scissors") ||
       (computerChoice === "Paper" && humanChoice === "Rock") ||
-      (computerChoice === "Sciccors" && humanChoice === "Paper")
+      (computerChoice === "Scissors" && humanChoice === "Paper")
     ) {
-      console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
       computerScore++;
+      result = `You lose! ${computerChoice} beats ${humanChoice}
+        Computer: ${computerScore}
+        Human: ${humanScore}
+        Ties: ${ties}`;
     } else if (humanChoice === computerChoice) {
-      console.log(`A tie!`);
       ties++;
+      result = `A tie!
+        Computer: ${computerScore}
+        Human: ${humanScore}
+        Ties: ${ties}`;
     } else {
-      console.log(`You win! ${humanChoice} beats ${computerChoice}`);
       humanScore++;
-    }
-  }
+      result = `You win! ${humanChoice} beats ${computerChoice}
+        Computer: ${computerScore}
+        Human: ${humanScore}
+        Ties: ${ties}`;
+      }
+      
+  resultDiv.innerText = result;
 
-  let count = 0;
-  while (count < 5) {
-    const human = getHumanChoice();
-    const computer = getComputerChoice();
-    playRound(human, computer);
-    count++;
+  if (computerScore === 5) {
+    end.innerText = "Robots won!";
+    buttons.forEach((button) => {
+      button.removeEventListener("click", playRound);
+    });
+    resetGame();
+  } else if (humanScore === 5) {
+    end.innerText = "Humans won!";
+    buttons.forEach((button) => {
+      button.removeEventListener("click", playRound);
+    });
+    resetGame();
   }
-  console.log(
-    `Computer: ${computerScore}\nHuman: ${humanScore}\nTies: ${ties}`
-  );
 }
-
-playGame();
